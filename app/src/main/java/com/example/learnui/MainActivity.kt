2d31390ca31e6b8855742ffe2cde13454fc12483
Haggai Khafizov <hafyok5777@gmail.com>
2023-12-19 17:04:31 +0300

    Finished wallet section

--- app/src/main/java/com/example/learnui/MainActivity.kt
+++ app/src/main/java/com/example/learnui/MainActivity.kt
@@ -9,15 +9,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.learnui.ui.theme.LearnUITheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,7 +40,7 @@ fun HomeScreen(){
                 .fillMaxSize()
                 .padding(padding)
         ){
-            //WalletSection()
+            WalletSection()
             //CardSection()
              Spacer(modifier = Modifier.height(16.dp))
             //FinanceSection()
