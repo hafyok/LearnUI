4e4ae77d8047ab2ffa01755b8c894c0725cc73ce
Haggai Khafizov <hafyok5777@gmail.com>
2024-01-10 14:18:48 +0300

    Beta-version

--- app/src/main/java/com/example/learnui/MainActivity.kt
+++ app/src/main/java/com/example/learnui/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.learnui.Presentation.CardsSection
+import com.example.learnui.Presentation.CurrenciesSection
 import com.example.learnui.Presentation.FinanceSection
 import com.example.learnui.Presentation.WalletSection
 
@@ -47,7 +48,7 @@ fun HomeScreen(){
             CardsSection()
             Spacer(modifier = Modifier.height(16.dp))
             FinanceSection()
-            //CurrentSection()
+            CurrenciesSection()
         }
     }
 }
